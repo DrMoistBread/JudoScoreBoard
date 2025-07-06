72442ff5aab46f1e73b8cc9d29e9f1db704f1950
Diogo Cró <diogocror@gmail.com>
2025-07-06 22:29:52 +0100

    increase and decrease timer

---
+++ JudoScoreBoard/Components/SimpleModal.razor.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JudoScoreBoard.Components;
+
+public class SimpleModal_razor : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- JudoScoreBoard/Program.cs
+++ JudoScoreBoard/Program.cs
@@ -1,10 +1,13 @@
 using JudoScoreBoard.Components;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHotKeys2();
 
 var app = builder.Build();
 
